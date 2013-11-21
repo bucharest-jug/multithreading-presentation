62b105ca6c3700778e39b031137e1bf03750da72
Alexandru Nedelcu <me@alexn.org>
2013-11-21 17:51:42 +0200

    Initial commit

--- src/main/java/example2/ThreadingPools.java
+++ src/main/java/example2/ThreadingPools.java
@@ -1,14 +1,17 @@
 package example2;
 
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ThreadingPools {
     public static void main(String[] args) {
         ForkJoinPool pool = new ForkJoinPool();
+        final AtomicInteger ref = new AtomicInteger(0);
 
         pool.execute(new Runnable() {
             public void run() {
-                System.out.println("Hello, World!");
+                String msg = String.format("Hello, World (#%d) !", ref.incrementAndGet());
+                System.out.println(msg);
             }
         });
 
