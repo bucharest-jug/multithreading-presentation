839278d71f828d96125958fdd0f0367b206a5214
Alexandru Nedelcu <me@alexn.org>
2013-11-21 17:54:20 +0200

    Fix

--- src/main/java/example2/ThreadingPools.java
+++ src/main/java/example2/ThreadingPools.java
@@ -1,20 +1,28 @@
 package example2;
 
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class ThreadingPools {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ForkJoinPool pool = new ForkJoinPool();
         final AtomicInteger ref = new AtomicInteger(0);
 
-        pool.execute(new Runnable() {
+        Runnable r = new Runnable() {
             public void run() {
                 String msg = String.format("Hello, World (#%d) !", ref.incrementAndGet());
                 System.out.println(msg);
             }
-        });
+        };
 
+        pool.execute(r);
+        pool.execute(r);
+        pool.execute(r);
+        pool.execute(r);
+
+        pool.shutdown();
+        pool.awaitTermination(1, TimeUnit.MINUTES);
         pool.shutdown();
     }
 }
